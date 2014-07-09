13ae5681a9ff98f585d91c795c28abdaebdbe43b
Ken Mugrage <kmugrage@thoughtworks.com>
2014-07-09 16:53:08 -0700

    new repo

---
+++ gocd-site/manifests/site.pp
@@ -0,0 +1,24 @@
+exec { 'apt-get update':
+	command => '/usr/bin/apt-get update';
+}
+
+package {'ruby1.9.3' :
+	ensure 	=>	'installed',
+	require	=>	Exec['apt-get update'],
+	before	=>	Package['jekyll'],
+}
+
+package {'git' :
+	ensure => 'latest',
+	require	=>	Exec['apt-get update'],
+}
+
+package {'make' :
+	ensure => 'latest',
+	require	=>	Exec['apt-get update'],
+}
+
+package {'jekyll' :
+	ensure => 'latest',
+	provider => 'gem',
+}
